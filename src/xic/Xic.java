91e52a9c720ca5392b0a65daab3cfcc5a32e6ce7
yuchien <yc2257@cornell.edu>
2016-02-17 12:55:10 -0500

    Clean up Xic for command line option

--- src/xic/Xic.java
+++ src/xic/Xic.java
@@ -1,26 +1,17 @@
 package xic;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-
-import java_cup.runtime.Symbol;
-
 import org.apache.commons.cli.HelpFormatter;
 
 
-import java.util.Map;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-
-
+import java_cup.runtime.ComplexSymbolFactory.ComplexSymbol;
 
 /**
  * @author yuchien
@@ -30,50 +21,58 @@ public class Xic {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception{
 		CommandLineParser parser = new DefaultParser();
 		Options options = new Options();
 		options.addOption( "h", "help", false, "Print a synopsis of options" );
 		options.addOption( "l", "lex", false, "Generate output from lexical analysis" );
 		options.addOption( "p", "parse", false, "Generate output from syntactic analysis" );
 
-		CommandLine commandLine;
-		try {
-			commandLine = parser.parse(options, args);
-			final String[] remainingArguments = commandLine.getArgs();
+		CommandLine commandLine = parser.parse(options, args);
+		final String[] remainingArguments = commandLine.getArgs();
+		
+		if(commandLine.hasOption("h")) {
 			HelpFormatter formatter = new HelpFormatter();
-//			formatter.printHelp( "xic", options );
-			
+			formatter.printHelp( "xic [options] <source files>", options );
+		} else {
 			String pathXi = remainingArguments[0];
 			String pathLexed = pathXi.substring(0,pathXi.length()-2)+"lexed";
+			String pathParsed = pathXi.substring(0,pathXi.length()-2)+"parsed";
 			
-			Reader fr = new FileReader(pathXi);
-//			FileWriter fw = new FileWriter(pathLexed);
-			
-			Lexer lexer = new Lexer(fr);
-			
-//			parser p = new parser(lexer, sf);
-			System.out.print(String.format("Compiling: %s\n", pathXi));
-			try {
+			System.out.print(String.format("Compiling: %s\n", pathXi));				
 
+			if(commandLine.hasOption("l")) {
+				
+				Reader fr = new FileReader(pathXi);
+				Lexer lexer = new Lexer(fr);
+				FileWriter fw = new FileWriter(pathLexed);
+	            for (ComplexSymbol symbol = (ComplexSymbol) lexer.next_token(); symbol.sym != 0; symbol = (ComplexSymbol) lexer.next_token()) {
+	            	String line = String.format("%d:%d %s", symbol.getLeft().getLine(), symbol.getLeft().getColumn(), symbol.getName());
+	            	if(symbol.value() != null){
+	            		line += " " + symbol.value() + "\n";
+	            	} else {
+	            		line += "\n";
+	            	}
+	            	System.out.print(line);
+	            	fw.write(line);
+	            }
+	            
+	            fw.close();
+	            
+			} 
+			
+			if(commandLine.hasOption("p")) {
+				Reader fr = new FileReader(pathXi);
+				Lexer lexer = new Lexer(fr);
+				FileWriter fw = new FileWriter(pathParsed);
 				parser p = new parser(lexer);
-	            System.out.println(p.parse().value);								
-
-//	            for (Symbol sym = lexer.next_token(); sym.sym != 0; sym = lexer.next_token()) {
-//	                System.out.println(String.format("(%d, %d) | Token %s: %s ", 
-//	                				sym.left, sym.right, sym, sym.value));
-//	            }
-
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-						
-		} catch (ParseException e) {
-			System.out.println( "Unexpected exception:" + e.getMessage() );
-		}
-	
-
-		
+				String program = p.parse().value.toString();
+	            System.out.println(program);
+				fw.write(program);
+	            fw.close();
+			}	
+		}		
 	}
 }
